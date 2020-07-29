973af69af7c3b5c75bb5a9b82151c1bd86862214
AlbertXIII <jorge.alberto.rc@gmail.com>
2020-07-29 16:48:31 -0500

    small format

--- src/components/ContextC.js
+++ src/components/ContextC.js
@@ -3,7 +3,6 @@ import ContextD from './ContextD'
 import ComicContext from './comicContext'
 
 class ContextC extends Component {
-
     //another option to use context, but this only works with class components and can only used in 1
     //static contextType = ComicContext
     render(){
